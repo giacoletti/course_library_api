bd804e56443a2f2bde78bd02906f238e4ceb0b22
Giovanni Iacoletti <giovanni.iacoletti@addiva.se>
2023-09-05 11:37:48 +0200

    Update PartiallyUpdateCourseForAuthor action to allow upserting

--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -120,7 +120,18 @@ public class CoursesController : ControllerBase
 
         if (courseForAuthorFromRepo is null)
         {
-            return NotFound();
+            var courseDto = new CourseForUpdateDto();
+            patchDocument.ApplyTo(courseDto);
+            var courseToAdd = _mapper.Map<Entities.Course>(courseDto);
+            courseToAdd.Id = courseId;
+
+            _courseLibraryRepository.AddCourse(authorId, courseToAdd);
+            await _courseLibraryRepository.SaveAsync();
+
+            var courseToReturn = _mapper.Map<CourseDto>(courseToAdd);
+            return CreatedAtRoute("GetCourseForAuthor",
+                new { authorId, courseId = courseToReturn.Id },
+                courseToReturn);
         }
 
         var courseToPatch = _mapper.Map<CourseForUpdateDto>(courseForAuthorFromRepo);
