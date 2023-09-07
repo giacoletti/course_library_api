2d48ee306a7c289674b1c181335631aeedd066eb
Giovanni Iacoletti <giovanni.iacoletti@addiva.se>
2023-09-07 11:10:19 +0200

    Refactor CourseForCreationDto and CourseForUpdateDto, add required Description to CourseForUpdateDto

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,14 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿namespace CourseLibrary.API.Models;
 
-namespace CourseLibrary.API.Models;
-
-public class CourseForCreationDto
+public class CourseForCreationDto : CourseForManipulationDto
 {
-    [Required(ErrorMessage = "You should fill out a title.")]
-    [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters.")]
-    public string Title { get; set; } = string.Empty;
-    [MaxLength(1500, ErrorMessage = "The description shouldn't have more than 1500 characters.")]
-    public string Description { get; set; } = string.Empty;
+
 }
 
- 
