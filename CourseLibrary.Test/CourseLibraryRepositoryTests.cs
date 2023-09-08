8eb09a9c16078f4e263c378d93e147ab966bd1b5
Giovanni Iacoletti <giovanni.iacoletti@addiva.se>
2023-09-08 16:53:58 +0200

    Create CourseLibraryRepositoryTests, add unit tests: GetAuthorsAsync must return a list of authors, GetAuthorAsync must return a single author, GetAuthorAsync passing a nonexistent authorId must return null, GetAuthorAsync passing an empty Guid must throw an ArgumentNullException

---
+++ CourseLibrary.Test/CourseLibraryRepositoryTests.cs
@@ -0,0 +1,63 @@
+﻿using CourseLibrary.API.DbContexts;
+using CourseLibrary.API.Entities;
+using CourseLibrary.API.Services;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseLibrary.Test
+{
+    public class CourseLibraryRepositoryTests
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        public CourseLibraryRepositoryTests()
+        {
+            var connection = new SqliteConnection("Data Source=:memory:");
+            connection.Open();
+            var optionsBuilder = new DbContextOptionsBuilder<CourseLibraryContext>().UseSqlite(connection);
+            var dbContext = new CourseLibraryContext(optionsBuilder.Options);
+            dbContext.Database.Migrate();
+
+            _courseLibraryRepository = new CourseLibraryRepository(dbContext);
+        }
+
+        [Fact]
+        public async Task GetAuthorsAsync_MustReturnListOfAuthors()
+        {
+            // Act
+            var result = await _courseLibraryRepository.GetAuthorsAsync();
+
+            // Assert
+            Assert.IsType<List<Author>>(result);
+        }
+
+        [Fact]
+        public async Task GetAuthorAsync_ExistingAuthorId_MustReturnAuthor()
+        {
+            // Act
+            var result = await _courseLibraryRepository.GetAuthorAsync(Guid.Parse("d28888e9-2ba9-473a-a40f-e38cb54f9b35"));
+
+            // Assert
+            Assert.IsType<Author>(result);
+        }
+
+        [Fact]
+        public async Task GetAuthorAsync_NonexistentAuthorId_MustReturnNull()
+        {
+            // Act
+            var result = await _courseLibraryRepository.GetAuthorAsync(Guid.Parse("10000000-0000-0000-0000-000000000000"));
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task GetAuthorAsync_EmptyGuid_MustThrowArgumentNullException()
+        {
+            // Act & Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(
+                async () =>
+                await _courseLibraryRepository.GetAuthorAsync(Guid.Parse("00000000-0000-0000-0000-000000000000"))
+                );
+        }
+    }
+}
