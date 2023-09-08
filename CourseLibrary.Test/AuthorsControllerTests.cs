e556c0fc715d0de93e7be358a4ce268757262b38
Giovanni Iacoletti <giovanni.iacoletti@addiva.se>
2023-09-08 13:30:38 +0200

    Add unit test: GetAuthor action must return NotFound result when passing a nonexistent authorId

--- CourseLibrary.Test/AuthorsControllerTests.cs
+++ CourseLibrary.Test/AuthorsControllerTests.cs
@@ -4,6 +4,7 @@ using CourseLibrary.API.Entities;
 using CourseLibrary.API.Models;
 using CourseLibrary.API.Profiles;
 using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
@@ -11,7 +12,7 @@ namespace CourseLibrary.Test
 {
     public class AuthorsControllerTests
     {
-        private readonly Guid _authorIdMock = Guid.Parse("d28888e9-2ba9-473a-a40f-e38cb54f9b35");
+        private readonly Guid TestAuthorId = Guid.Parse("d28888e9-2ba9-473a-a40f-e38cb54f9b35");
         private readonly AuthorsController _authorsController;
 
         public AuthorsControllerTests()
@@ -26,11 +27,11 @@ namespace CourseLibrary.Test
                         new Author("Anne", "Adams", "Rum")
                     });
             courseLibraryRepositoryMock
-                .Setup(m => m.GetAuthorAsync(_authorIdMock))
+                .Setup(m => m.GetAuthorAsync(TestAuthorId))
                 .ReturnsAsync(
                         new Author("Jaimy", "Johnson", "Navigation")
                         {
-                            Id = _authorIdMock
+                            Id = TestAuthorId
                         }
                        );
 
@@ -55,11 +56,24 @@ namespace CourseLibrary.Test
         public async Task GetAuthor_GetAction_MustReturnOkObjectResult()
         {
             // Act
-            var result = await _authorsController.GetAuthor(_authorIdMock);
+            var result = await _authorsController.GetAuthor(TestAuthorId);
 
             // Assert
             var actionResult = Assert.IsType<ActionResult<AuthorDto>>(result);
-            Assert.IsType<OkObjectResult>(actionResult.Result);
+            var objectResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+            Assert.Equal(StatusCodes.Status200OK, objectResult.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetAuthor_GetActionWithNonexistentAuthorId_MustReturnNotFoundResult()
+        {
+            // Act
+            var result = await _authorsController.GetAuthor(Guid.Parse("00000000-0000-0000-0000-000000000000"));
+
+            // Assert
+            var actionResult = Assert.IsType<ActionResult<AuthorDto>>(result);
+            var objectResult = Assert.IsType<NotFoundResult>(actionResult.Result);
+            Assert.Equal(StatusCodes.Status404NotFound, objectResult.StatusCode);
         }
     }
 }
