67dd3b88639cd0fefb6c2baaec8126ed7d18e1bd
Giovanni Iacoletti <giovanni.iacoletti@addiva.se>
2023-09-15 11:22:42 +0200

    Add unit test: GetAuthors action passing pageNumber and pageSize must return expected list of authors

--- CourseLibrary.Test/AuthorsControllerTests.cs
+++ CourseLibrary.Test/AuthorsControllerTests.cs
@@ -15,6 +15,8 @@ namespace CourseLibrary.Test
         private readonly Guid TestAuthorId = Guid.Parse("d28888e9-2ba9-473a-a40f-e38cb54f9b35");
         private readonly string TestMainCategory = "Singing";
         private readonly string TestSearchQuery = "sea";
+        private readonly int TestPageNumber = 2;
+        private readonly int TestPageSize = 5;
         private readonly AuthorsController _authorsController;
 
         public AuthorsControllerTests()
@@ -80,6 +82,23 @@ namespace CourseLibrary.Test
                         {
                             Id = Guid.Parse("5b3621c0-7b12-4e80-9c8b-3398cba7ee05"),
                             DateOfBirth = new DateTime(1956, 11, 23)
+                        }
+                    });
+            courseLibraryRepositoryMock
+                .Setup(m => m.GetAuthorsAsync(
+                    It.Is<AuthorsResourceParameters>(p => p.PageNumber == TestPageNumber && p.PageSize == TestPageSize)))
+                .ReturnsAsync(
+                    new List<Author>()
+                    {
+                        new Author("Berry", "Griffin Beak Eldritch", "Ships")
+                        {
+                            Id = Guid.Parse("d28888e9-2ba9-473a-a40f-e38cb54f9b35"),
+                            DateOfBirth = new DateTime(1980, 7, 23)
+                        },
+                        new Author("Nancy", "Swashbuckler Rye", "Rum")
+                        {
+                            Id = Guid.Parse("da2fd609-d754-4feb-8acd-c4f9ff13ba96"),
+                            DateOfBirth = new DateTime(1978, 5, 21)
                         }
                     });
             courseLibraryRepositoryMock
@@ -167,6 +186,24 @@ namespace CourseLibrary.Test
             Assert.All(authorDtoList,
                 author => Assert.True(author.MainCategory.ToLower() == TestMainCategory.ToLower() && (author.Name.ToLower().Contains(TestSearchQuery)
                 || author.MainCategory.ToLower().Contains(TestSearchQuery))));
+        }
+
+        [Fact]
+        public async Task GetAuthors_GetActionWithPageNumberAndPageSize_MustReturnOkObjectResult()
+        {
+            // Act
+            var result = await _authorsController.GetAuthors(
+                new AuthorsResourceParameters()
+                {
+                    PageNumber = TestPageNumber,
+                    PageSize = TestPageSize
+                });
+
+            // Assert
+            var actionResult = Assert.IsType<ActionResult<IEnumerable<AuthorDto>>>(result);
+            var objectResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+            var authorDtoList = Assert.IsType<List<AuthorDto>>(objectResult.Value);
+            Assert.Equal(2, authorDtoList.Count());
         }
 
         [Fact]
